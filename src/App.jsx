2bc43fc7dfacb6dd7218a4ed918acd82db722799
indresh-dev-codes <indreshnune0571@gmail.com>
2026-03-27 12:17:17 +0530

    Updated App component with personal intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 function App() {
   return (
-<h1>Hello React</h1>
-  )
+    <div>
+      <h1>Hi, I am Indresh 👋</h1>
+      <p>Learning React step by step</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
