b70258b7463bb929af1ef44f47c295adb0060fba
indresh-dev-codes <indreshnune0571@gmail.com>
2026-03-27 13:43:00 +0530

    After adding the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const productApi = "https://dummyjson.com/products";
+
+  useEffect(() => {
+    fetch(productApi)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        console.log("data", data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Hi, I am Indresh 👋</h1>
-      <p>Learning React step by step</p>
-      <h2>Starting...</h2>
+      <p>Total products: {products.length}</p>
+
+      <h2>Products:</h2>
+
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {products.map((item) => {
+          return (
+            <div
+              key={item.id}
+              style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                width: "200px",
+                borderRadius: "10px",
+              }}
+            >
+              <img
+                src={item.thumbnail}
+                alt={item.title}
+                style={{ width: "100%", borderRadius: "10px" }}
+              />
+              <h3>{item.title}</h3>
+              <p>Price: ${item.price}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
